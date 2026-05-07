b004e8220d3893a158e0de364c2eb1d0546338a7
ati-n <50715221+ati-n@users.noreply.github.com>
2026-05-07 09:33:46 +0200

    config fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+    output: "static",
     integrations: [tailwind()],
     site: "[https://stillus.app](https://stillus.app)",
 });
